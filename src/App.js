ce2f6e657fe44d55e0afec9ca54dc70c30eb3fe7
hacerbulut <hcrleather@gmail.com>
2023-07-15 15:36:35 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+      <div className="header">
+        <h2>REACT Interview Ouestions And Answers</h2>
+      </div>
+      <div className="app">
+        <div className="main">
+        
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/InterviewAccord.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InterviewAccord = () => {
+  return (
+    <div>
+      InterviewAccord
+    </div>
+  )
+}
+
+export default InterviewAccord
